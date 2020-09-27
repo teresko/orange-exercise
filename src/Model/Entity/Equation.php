e9f1be2d10bb01295aa8fb5a46cf5bf19fdae46f
Mārtiņš Tereško <martins.teresko@gmail.com>
2020-09-27 16:54:40 +0300

    Mocking data for playing around with JS part of the application

--- src/Model/Entity/Equation.php
+++ src/Model/Entity/Equation.php
@@ -2,7 +2,28 @@
 
 namespace Model\Entity;
 
-class Equation
-{
+use Model\Contract\HasId;
 
+class Equation implements HasId
+{
+    private $id = 7;
+    private $expression = '2 + 3 * 4';
+    private $result = 14;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function getExpression(): ?string
+    {
+        return $this->expression;
+    }
+
+
+    public function getResult()
+    {
+        return $this->result;
+    }
 }
--- src/Model/Entity/EquationCollection.php
+++ src/Model/Entity/EquationCollection.php
@@ -7,6 +7,11 @@ use Component\Collection;
 
 class EquationCollection extends Collection
 {
+    public function __construct()
+    {
+        $this->addEntity(new Equation);
+    }
+
     protected function buildEntity(): HasId
     {
         return Equation;
